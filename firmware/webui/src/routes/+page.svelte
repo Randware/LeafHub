037fa9ddc955bdd1069555ca910cc5c4dca52b43
GHaxZ <ghax.z.yt@proton.me>
2025-04-08 08:21:08 +0200

    Adjust page size for mobile browsers

--- firmware/webui/src/routes/+page.svelte
+++ firmware/webui/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 	import PageContainer from '$lib/components/PageContainer.svelte';
 </script>
 
-<div class="h-screen w-screen">
+<div class="h-[100svh] w-[100svw]">
 	<div class="bg-secondary flex h-full w-full flex-col gap-6 p-6">
 		<PageContainer />
 	</div>
