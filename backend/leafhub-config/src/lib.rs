9c027e270596311df4ce26d2d733d3a3eecea439
Dari-OS <darius.dev@proton.me>
2025-04-17 02:55:43 +0200

    Changed the internally used "universal value" from erased_serde to serde_json

--- backend/leafhub-config/src/lib.rs
+++ backend/leafhub-config/src/lib.rs
@@ -1,5 +1,4 @@
 mod states;
-use erased_serde::Serialize as ErasedSerialize;
 use serde::{Deserialize, Serialize, de::DeserializeOwned};
 use serde_json::Value;
 use states::{Builder, ConfigState, FileFormat, Finished};
@@ -7,24 +6,33 @@ use std::{
     collections::HashMap,
     fs::OpenOptions,
     io::{self, Error, ErrorKind},
+    marker::PhantomData,
 };
 
-pub struct Config<State>
+pub struct Config<'cfg, State>
 where
     State: ConfigState,
 {
     state: State,
+    _lifetime: PhantomData<&'cfg ()>,
 }
 
-impl<'a, State: ConfigState> Config<State> {
-    pub fn new() -> Config<Builder<'a>> {
+impl<'cfg, State: ConfigState> Config<'cfg, State> {
+    pub fn new<'b>() -> Config<'cfg, Builder<'b>>
+    where
+        'cfg: 'b,
+    {
         Config {
             state: Builder::default(),
+            _lifetime: PhantomData,
         }
     }
 }
 
-impl<'a> Config<Builder<'a>> {
+impl<'cfg, 'b> Config<'cfg, Builder<'b>>
+where
+    'cfg: 'b,
+{
     /// Sets the FileFormat of the config file
     pub fn with_format(mut self, format: FileFormat) -> Self {
         self.state.format = Some(format);
@@ -32,8 +40,28 @@ impl<'a> Config<Builder<'a>> {
     }
 
     /// Sets the path where the config will be loaded/saved to
-    pub fn with_path(mut self, path: &'a str) -> Self {
+    pub fn with_path(mut self, path: &'b str) -> Self {
         self.state.path = Some(path);
+        self
+    }
+
+    /// If enabled overrides any existing *file* that lies at the set path and ignores its values
+    pub fn override_file(mut self, override_existing: bool) -> Self {
+        self.state.override_file = override_existing;
+        self
+    }
+
+    /// If enabled overrides stored values from the file with the set default values.
+    ///
+    /// # Example
+    /// So if there is a key/value like: __"key1" -> "val1"__ in the file and
+    /// the user provided a default value: __"key1" -> "super important val"__ the
+    /// value from the file gets overriden with the default one.
+    ///
+    /// This isn't the case normally, because default values
+    /// only get used if there are no values already stored with that key
+    pub fn override_stored(mut self, override_stored: bool) -> Self {
+        self.state.override_stored = override_stored;
         self
     }
 
@@ -48,7 +76,7 @@ impl<'a> Config<Builder<'a>> {
     pub fn add_default<S, F, T>(mut self, name: String, mapper: F) -> Self
     where
         F: Fn(&String) -> Option<T> + 'static,
-        T: ErasedSerialize + 'static,
+        T: Serialize + 'static,
     {
         // Initialize the HashMap if it isn't already (only the case on first time methode call)
         if self.state.default_values.is_none() {
@@ -58,44 +86,108 @@ impl<'a> Config<Builder<'a>> {
         // Executes the mapper
         let mapped_value = mapper(&name);
         // Do nothing when `None` gets returned
-        if mapped_value.is_none() {
-            return self;
-        }
-        let mut hash_map = self.state.default_values.unwrap();
-        hash_map.insert(name, Box::new(mapped_value.unwrap()));
-        self.state.default_values = Some(hash_map);
-        self
-    }
-
-    // If enabled overrides any existing file that lies at the set path
-    pub fn override_existing(mut self, override_existing: bool) -> Self {
-        self.state.override_existing = override_existing;
-        self
-    }
-
-    pub fn finish(mut self) -> Result<Config<Finished>, Error> {
-        // Checks if the user set a path
-        if let Some(path) = self.state.path {
-            let mut config = OpenOptions::new();
-            config.read(true).write(true);
-
-            // Set the correct flags for the config
-            if self.state.override_existing {
-                config.truncate(true);
+        if let Some(val) = mapped_value {
+            // Serialize the value into json for an universal type.
+            // If that fails do nothing
+            let mapped_value = if let Ok(valid_value) = serde_json::to_value(val) {
+                valid_value
             } else {
-                config.create(true);
-            }
+                return self;
+            };
 
-            let config = config.open(self.state.path.unwrap())?;
-
-            //TODO: Finish up the state transition
+            let mut hash_map = self.state.default_values.unwrap();
+            hash_map.insert(name, mapped_value);
+            self.state.default_values = Some(hash_map);
+            self
         } else {
-            return Err(Error::new(
-                ErrorKind::InvalidInput,
-                "Provide a path to the Config",
-            ));
+            self
+        }
+    }
+
+    pub fn finish<'f>(mut self) -> Result<Config<'cfg, Finished<'f>>, Error>
+    where
+        'cfg: 'f,
+    {
+        // Checks if the user set a path
+        let path = self
+            .state
+            .path
+            .ok_or_else(|| Error::new(ErrorKind::InvalidInput, "Provide a path to the Config"))?;
+
+        let format = self.state.format.unwrap_or(FileFormat::Toml);
+
+        let mut config = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .truncate(self.state.override_file)
+            .open(path)?;
+
+        // Load the values from the actual file if the user wants to ignore the old ones
+        let mut values = if self.state.override_file {
+            HashMap::new()
+        } else {
+            //TODO: DOCUMENT THAT THE CONFIG FILE GETS IGNORED WHEN IT CAN NOT BE DESERIALIZED!!!
+            format.deserialize(&mut config).unwrap_or_default()
+        };
+
+        if let Some(defaults) = self.state.default_values {
+            for (key, val) in defaults {
+                if !values.contains_key(&key) || self.state.override_stored {
+                    //TODO: Document the panic
+                    values.insert(
+                        key,
+                        serde_json::to_value(val)
+                            .expect("The given default could not be serialized"),
+                    );
+                }
+            }
         }
 
-        todo!();
+        Ok(Config {
+            state: Finished {
+                _lifetime: PhantomData,
+                values: values,
+                file: config,
+            },
+            _lifetime: PhantomData,
+        })
+    }
+}
+
+impl<'cfg, 'f> Config<'cfg, Finished<'f>>
+where
+    'cfg: 'f,
+{
+    pub fn get<T: DeserializeOwned>(&self, key: &str) -> Option<T> {
+        self.state
+            .values
+            .get(key)
+            .and_then(|val| serde_json::from_value(val.clone()).ok())
+    }
+
+    /// Adds a given key and value to the config
+    ///
+    /// # Panics
+    /// This function panics if the given can not be serialized.
+    /// To avoid a panic use [`try_add`](fn@try_add) to get an Error instead
+    pub fn add<T: Serialize>(&mut self, key: &str, value: T) {
+        self.state.values.insert(
+            key.to_string(),
+            serde_json::to_value(value).expect("Could not serialize given value"),
+        );
+    }
+
+    /// Adds a given key and value to the config
+    ///
+    /// # Returns
+    ///
+    /// - `Ok(())`: when everything went fine
+    /// - `Err(serde_json::Error)`: When the type could not be serialized
+    pub fn try_add<T: Serialize>(&mut self, key: &str, value: T) -> Result<(), serde_json::Error> {
+        self.state
+            .values
+            .insert(key.to_string(), serde_json::to_value(value)?);
+        Ok(())
     }
 }
--- backend/leafhub-config/src/states/mod.rs
+++ backend/leafhub-config/src/states/mod.rs
@@ -1,41 +1,76 @@
-use erased_serde::Serialize as ErasedSerialize;
 use serde_json::Value;
-use std::{collections::HashMap, fs::File, path::Path};
+use std::{
+    collections::HashMap,
+    fs::File,
+    io::{self, Read},
+    marker::PhantomData,
+    path::Path,
+};
 
 pub trait ConfigState {}
 
 #[derive(Default)]
-pub struct Builder<'a> {
-    pub(crate) path: Option<&'a str>,
+pub struct Builder<'b> {
+    pub(crate) path: Option<&'b str>,
     pub(crate) format: Option<FileFormat>,
-    pub(crate) default_values: Option<HashMap<String, Box<dyn ErasedSerialize>>>,
-    pub(crate) override_existing: bool,
+    pub(crate) default_values: Option<HashMap<String, serde_json::Value>>,
+    pub(crate) override_file: bool,
+    pub(crate) override_stored: bool,
 }
 
-impl<'a> Builder<'a> {
+impl<'b> Builder<'b> {
     #[allow(dead_code)]
-    pub(crate) fn new(path: &'a str, format: FileFormat) -> Self {
+    pub(crate) fn new(path: &'b str, format: FileFormat) -> Self {
         Builder {
             path: Some(path),
             format: Some(format),
             default_values: None,
-            override_existing: false,
+            override_file: false,
+            override_stored: false,
         }
     }
 }
 
 impl ConfigState for Builder<'_> {}
 
-pub struct Finished {
-    file: File,
-    values: HashMap<String, Box<dyn ErasedSerialize>>,
+pub struct Finished<'f> {
+    pub(crate) _lifetime: PhantomData<&'f ()>,
+    pub(crate) file: File,
+    pub(crate) values: HashMap<String, serde_json::Value>,
 }
 
-impl ConfigState for Finished {}
+impl ConfigState for Finished<'_> {}
 
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum FileFormat {
     Yaml,
     Toml,
     Json,
+    Cbor,
+    MessagePack,
+    Pickle,
+    Ron,
+    Bson,
+    Avro,
+    Json5,
+    Url,
+}
+
+impl FileFormat {
+    pub fn deserialize<R: Read>(&self, reader: R) -> io::Result<HashMap<String, Value>> {
+        let map = match self {
+            FileFormat::Yaml => todo!(),
+            FileFormat::Toml => todo!(),
+            FileFormat::Json => todo!(),
+            FileFormat::Cbor => todo!(),
+            FileFormat::MessagePack => todo!(),
+            FileFormat::Pickle => todo!(),
+            FileFormat::Ron => todo!(),
+            FileFormat::Bson => todo!(),
+            FileFormat::Avro => todo!(),
+            FileFormat::Json5 => todo!(),
+            FileFormat::Url => todo!(),
+        };
+        todo!()
+    }
 }
