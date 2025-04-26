92706baa63f6370f321287773bc11dd5e5c40109
GHaxZ <ghax.z.yt@proton.me>
2025-04-26 19:58:53 +0200

    Add visual indicator for blocked Next and Back buttons

--- firmware/webui/src/lib/components/PageContainer.svelte
+++ firmware/webui/src/lib/components/PageContainer.svelte
@@ -63,14 +63,19 @@
 
 <footer class="flex w-full justify-between">
 	<button
-		class="text-dark bg-primary hover:bg-primary/70 rounded-xl px-4 py-2 text-xl font-semibold transition-colors"
+		class="text-dark {firstPage
+			? 'bg-gray-400/80'
+			: 'bg-primary hover:bg-primary/70'} rounded-xl px-4 py-2 text-xl font-semibold transition-colors"
 		onclick={prev}
 		disabled={firstPage}
 	>
 		Back
 	</button>
+
 	<button
-		class="text-dark bg-primary hover:bg-primary/70 rounded-xl px-4 py-2 text-xl font-semibold transition-colors"
+		class="text-dark {currentStep.block || lastPage
+			? 'bg-gray-400/80'
+			: 'bg-primary hover:bg-primary/70 '} rounded-xl px-4 py-2 text-xl font-semibold transition-colors"
 		onclick={next}
 		disabled={currentStep.block || lastPage}
 	>
